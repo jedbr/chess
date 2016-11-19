a414c49518a97a1940975897e420814e7be8172a
jedrzej bryll <jedrzej.bryll@gmail.com>
2016-11-19 03:00:51 +0100

    Implement board printing

--- lib/board.rb
+++ lib/board.rb
@@ -1,24 +1,50 @@
 class Board
-  attr_accessor :column
+  attr_accessor :position
 
   def initialize
-    @column = Hash.new { |h, k| h[k] = Array.new(9) }
+    @position = Hash.new { |h, k| h[k] = Array.new(9) }
     ("a".."h").each do |i|
-      @column[i.to_sym]
+      @position[i]
     end
   end
 
   def row(i)
     row = []
-    @column.each_value { |v| row << v[i] }
+    @position.each_value { |v| row << v[i] }
     row
   end
 
-  def setup
+  def print
+    system("clear")
+    puts "  ┌────┬────┬────┬────┬────┬────┬────┬────┐"
 
+    (1..8).each do |i|
+      to_print = row(i).map{ |s| s.nil? ? ("  ") : "#{s.symbol} "}.join(" │ ")
+      puts "#{9 - i} │ #{to_print} │"
+      puts "  ├────┼────┼────┼────┼────┼────┼────┼────┤" unless i == 8
+    end
+
+    puts "  └────┴────┴────┴────┴────┴────┴────┴────┘"
+    puts "    A    B    C    D    E    F    G    H  "
   end
 
-  def print
+  def setup
+    setup_pawns
+    setup_figures
+  end
+
+  private
+
+  def setup_pawns
+    @position.each do |c, r|
+      position = c + "2"
+      r[2] = Piece.create(:white, :pawn, position)
+      position = c + "7"
+      r[7] = Piece.create(:black, :pawn, position)
+    end
+  end
+
+  def setup_figures
 
   end
 end
